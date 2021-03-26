98f6fab9ea8c246d8f031ca81556996a31d0b3b9
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-26 15:07:48 -0300

    Mudando os html de pasta

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ const server = express()
 
 // request, response
 server.get('/', (request, response) => {
-   return response.send('SLV!') // send significa envia
+   // return response.sendFile('../index.html') // send significa envia
 })
 server.listen(3000, () => console.log('rodando'))
