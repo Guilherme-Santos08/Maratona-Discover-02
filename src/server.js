588027c7d0921879cd090dce47a84ae4b15e4fa5
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-26 15:22:20 -0300

    adicionando o express.static

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const server = express()
 
+// habilitar arquivos statics
+server.use(express.static("public"))
+
 // request, response
 server.get('/', (request, response) => {
-   return response.sendFile(__dirname + "/view/index.html") // send significa envia
+   return response.sendFile(__dirname + "/views/index.html") // send significa envia
 })
 server.listen(3000, () => console.log('rodando'))
