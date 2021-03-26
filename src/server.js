0155060fe58d0767e1e043298f9e6d5f63302ddf
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-26 15:09:29 -0300

    Colocando html na pasta view

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ const server = express()
 
 // request, response
 server.get('/', (request, response) => {
-   // return response.sendFile('../index.html') // send significa envia
+   return response.sendFile(__dirname + "/view/index.html") // send significa envia
 })
 server.listen(3000, () => console.log('rodando'))
